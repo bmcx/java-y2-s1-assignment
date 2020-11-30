220cd5f24d5ae8b50117b0a47a47c61cdcc15b8e
Chandima Bandara <bmcbandara@hotmail.com>
2020-11-30 23:41:28 +0530

    Update medcenter.sql

--- MedCenter/medcenter.sql
+++ MedCenter/medcenter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2020 at 07:57 AM
+-- Generation Time: Nov 30, 2020 at 07:11 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -32,7 +32,7 @@ CREATE TABLE `booking` (
   `doctorId` int(11) NOT NULL,
   `studentId` int(11) NOT NULL,
   `scheduleId` int(11) NOT NULL,
-  `status` enum('ACCEPTED','PENDING','CANCELED','REJECTED') NOT NULL,
+  `status` enum('ACCEPTED','PENDING','CANCELED','REJECTED') NOT NULL DEFAULT 'PENDING',
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,8 +41,12 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`id`, `doctorId`, `studentId`, `scheduleId`, `status`, `createdAt`) VALUES
-(1, 2, 3, 1, 'PENDING', '2020-11-29 05:22:07'),
-(2, 2, 4, 1, 'PENDING', '2020-11-29 05:22:07');
+(1, 210002, 210003, 1, 'REJECTED', '2020-11-29 05:22:07'),
+(2, 210002, 210004, 1, 'PENDING', '2020-11-29 05:22:07'),
+(4, 210002, 210004, 2, 'CANCELED', '2020-11-29 19:02:50'),
+(5, 210002, 210004, 7, 'CANCELED', '2020-11-30 18:02:34'),
+(6, 210002, 210004, 7, 'CANCELED', '2020-11-30 18:07:19'),
+(7, 210002, 210004, 7, 'PENDING', '2020-11-30 18:07:25');
 
 -- --------------------------------------------------------
 
@@ -64,8 +68,11 @@ CREATE TABLE `schedule` (
 --
 
 INSERT INTO `schedule` (`id`, `doctorId`, `day`, `timeFrom`, `timeTo`, `maxCount`) VALUES
-(1, 2, '2020-11-29', '10:00:00', '12:00:00', 5),
-(2, 2, '2020-11-29', '13:00:00', '15:00:00', 5);
+(1, 210002, '2020-11-25', '10:00:00', '12:00:00', 5),
+(2, 210002, '2020-11-30', '13:00:00', '15:00:00', 5),
+(3, 210002, '2020-11-25', '08:00:00', '12:00:00', 1),
+(4, 210002, '2020-11-30', '08:00:00', '09:00:00', 5),
+(7, 210002, '2020-12-02', '08:00:00', '10:00:00', 5);
 
 -- --------------------------------------------------------
 
@@ -90,10 +97,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `role`, `firstname`, `lastname`, `title`, `description`, `createdAt`) VALUES
-(1, 'test', '12345', 'STAFF', 'john', 'doe', '-', '-', '2020-11-28 18:13:56'),
-(2, 'doc', '12345', 'DOCTOR', 'Jane', 'Doe', 'Specialized in Counseling', 'Lorem ipsum dolor sit amet', '2020-11-29 05:18:07'),
-(3, 'stu1', '12345', 'STUDENT', 'Grant', 'Tanner', '', '', '2020-11-29 05:18:07'),
-(4, 'stu2', '12345', 'STUDENT', 'Kasper', 'Lennon', '', '', '2020-11-29 05:22:51');
+(210001, 'test', '12345', 'STAFF', 'john', 'doe', '-', '-', '2020-11-28 18:13:56'),
+(210002, 'doc2', '12345', 'DOCTOR', 'Jane', 'Doe', 'Specialized in Counseling', 'Lorem ipsum dolor sit amet', '2020-11-29 05:18:07'),
+(210003, 'stu1', '12345', 'STUDENT', 'Grant', 'Tanner', '', '', '2020-11-29 05:18:07'),
+(210004, 'stu2', '12345', 'STUDENT', 'Kasper', 'Lennon', '', '', '2020-11-29 05:22:51'),
+(210005, 'doc1', '12345', 'DOCTOR', 'Brenda', 'Horner', 'Orthopedic surgeon', 'Specializes in sports injuries, broken bones etc..', '2020-11-30 07:02:52');
 
 --
 -- Indexes for dumped tables
@@ -109,7 +117,8 @@ ALTER TABLE `booking`
 -- Indexes for table `schedule`
 --
 ALTER TABLE `schedule`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `timeFrom` (`timeFrom`,`timeTo`);
 
 --
 -- Indexes for table `user`
@@ -125,19 +134,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `schedule`
 --
 ALTER TABLE `schedule`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=210006;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
